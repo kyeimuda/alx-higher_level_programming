da1acda8ea7eb8498007f01ccfda2b0df54c6ac2
kyeimuda <kyeibernard01@gmail.com>
2021-11-17 16:47:07 -0800

    fix indentation

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for number in range(1, 101):
-        if number % 5  == 0 and number % 3 == 0:
+        if number % 5 == 0 and number % 3 == 0:
             print('FizzBuzz', end=" ")
-        elif number % 5  == 0:
+        elif number % 5 == 0:
             print('Buzz', end=" ")
         elif number % 3 == 0:
             print('Fizz', end=" ")
