558bb871169696e79c1e73ae345ff938e9711553
kyeimuda <kyeibernard01@gmail.com>
2021-11-18 17:00:16 -0800

    corrected import line

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    from sys import argv
     length = len(argv)
     added = 0
     if length == 1:
-        print("{:d}",.format(0))
+        print("{:d}".format(0))
     if length > 1:
         for index in range(1, length):
             added += int(argv[index])
