f65436cf3957628b35b7f14bb1d9a3b8cda8ac7d
kyeimuda <kyeibernard01@gmail.com>
2021-11-20 17:10:11 -0800

    python program to retrive an element from a list

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    length = len(my_list)
+    if idx < 0:
+        return None
+    elif idx >= length:
+        return None
+    elif idx >= 0 and idx < length:
+        return my_list[idx]
