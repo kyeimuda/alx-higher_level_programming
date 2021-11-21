119e20ba548b2e8e7b58d4219fd326ba97c5c9ed
kyeimuda <kyeibernard01@gmail.com>
2021-11-20 18:20:03 -0800

    python program replaces an element in a list at a specific position without modifying the original list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python03
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+    if idx < 0:
+        return my_list
+    elif idx >= length:
+        return my_list
+    elif idx >= 0 and idx < length:
+        new = my_list[:]
+        change = new[idx] = element
+        return new
