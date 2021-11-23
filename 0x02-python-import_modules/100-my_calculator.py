46c7aaf42d96e800f7eb7097526a8f7efb05438f
kyeimuda <kyeibernard01@gmail.com>
2021-11-22 16:55:19 -0800

    python program that imports all functions from the file calculator_1.py and handles basic operations.

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,19 +3,25 @@ if __name__ == "__main__":
     import calculator_1
     from sys import argv
     length = len(argv)
-    value1 = int(argv[1])
-    value2 = int(argv[3])
     if length != 4:
         print("""Usage: ./100-my_calculator.py <a> <operator> <b>""")
         exit(1)
-    elif argv[2] != '+' or argv[2] != '-' or argv[2] != '*' or argv[2] != '/':
+    elif argv[2] != '+' and argv[2] != '-' and argv[2] != '*' and argv[2] != '/':
         print("""Unknown operator. Available operators: +, -, * and /""")
         exit(1)
     elif argv[2] == '+':
+        value1 = int(argv[1])
+        value2 = int(argv[3])
         print("{:d} + {:d} = {:d}".format(value1, value2, calculator_1.add(value1, value2)))
     elif argv[2] == '-':
-        print("{:d} + {:d} = {:d}".format(value1, value2, calculator_1.sub(value1, value2)))
+        value1 = int(argv[1])
+        value2 = int(argv[3])
+        print("{:d} - {:d} = {:d}".format(value1, value2, calculator_1.sub(value1, value2)))
     elif argv[2] == '*':
-        print("{:d} + {:d} = {:d}".format(value1, value2, calculator_1.mul(value1, value2)))
+        value1 = int(argv[1])
+        value2 = int(argv[3])
+        print("{:d} * {:d} = {:d}".format(value1, value2, calculator_1.mul(value1, value2)))
     elif argv[2] == '/':
-        print("{:d} + {:d} = {:d}".format(value1, value2, calculator_1.div(value1, value2)))
+        value1 = int(argv[1])
+        value2 = int(argv[3])
+        print("{:d} / {:d} = {:d}".format(value1, value2, calculator_1.div(value1, value2)))
