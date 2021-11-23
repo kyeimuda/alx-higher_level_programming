b448f3f13ad9bf162e32690e02eb4de8a1c372cc
kyeimuda <kyeibernard01@gmail.com>
2021-11-23 12:20:24 -0800

    correction with str.format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for index in matrix:
-        print(*index, sep=" ")
+        length = len(index)
+        for subindex in index:
+            if subindex == index[-1]:
+                print("{:d}".format(subindex), end="")
+            else:
+                print("{:d}".format(subindex), end=" ")
+        print()
+            
