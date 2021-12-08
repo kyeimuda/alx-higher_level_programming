8417c1f31f07a2328150cc95b658ee31a9409f0e
kyeimuda <kyeibernard01@gmail.com>
2021-12-07 17:51:37 -0800

    documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,5 +6,5 @@ class Square:
     """Square class defined."""
 
     def __init__(self, size):
-        """Initialize a new Square.""
+        """Initialize a new Square."""
         self.__size = size
