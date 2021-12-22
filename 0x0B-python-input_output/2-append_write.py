c9d4b00f1fcef89a43f3f75a4dbe30e2e963cb0d
kyeimuda <kyeibernard01@gmail.com>
2021-12-21 17:21:49 -0800

    2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -17,4 +17,4 @@ def append_write(filename="", text=""):
     """
     with open(filename, "a", encoding="utf-8") as f:
         characters = f.write(text)
-        return (charaters)
+        return (characters)
