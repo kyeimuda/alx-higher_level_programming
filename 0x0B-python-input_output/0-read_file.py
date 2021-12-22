9cbef221499ba153c9b4ac6ce21de853c4d0d07e
kyeimuda <kyeibernard01@gmail.com>
2021-12-21 17:01:36 -0800

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-read_file.py
+"""
+This module"defines a text file-reading function.
+"""
+
+
+def read_file(filename=""):
+    """
+    function that reads a text file (UTF8) and\
+     prints it to stdout
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
