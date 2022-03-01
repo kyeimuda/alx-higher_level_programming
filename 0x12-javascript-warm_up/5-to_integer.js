1a890d117310d9547a193e69b1cad80ccedfcb95
kyeimuda <kyeibernard01@gmail.com>
2022-02-28 18:35:40 -0800

    5-to_integer.js

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// This script print a message if the first argument can be converted to an integer
+const myVar = process.argv[2];
+if (isNaN(myVar)) {
+  console.log('Not a number');
+} else {
+  console.log(`My number: ${parseInt(myVar)}`);
+}
