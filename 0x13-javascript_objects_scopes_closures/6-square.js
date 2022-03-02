db8e4a866ac56c7c886634205add93ba5f3e55c2
kyeimuda <kyeibernard01@gmail.com>
2022-03-01 18:36:43 -0800

    6-square.js

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+//class Square that defines a square and inherits from Square of 5-square.js
+const Square1 = require('./5-square');
+
+class Square extends Square1 {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let x = 0; x < this.height; x++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+module.exports = Square;
