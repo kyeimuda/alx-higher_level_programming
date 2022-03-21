bfdcc1c086458eae78a0f1bc5fcc99266bcf39fc
kyeimuda <kyeibernard01@gmail.com>
2022-03-20 21:18:12 -0700

    9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,6 +3,6 @@
 const firstNum = parseInt(process.argv[2]);
 const secondNum = parseInt(process.argv[3]);
 function add (a, b) {
-    return a + b;
+  return a + b;
 }
 console.log(add(firstNum, secondNum));
