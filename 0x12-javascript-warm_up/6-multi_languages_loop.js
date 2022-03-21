b783082ea9927ad5250aee1a967c25837da80a1c
kyeimuda <kyeibernard01@gmail.com>
2022-03-20 21:12:40 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // This script print 3 lines but by using an array of string and a loop
-const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const i of myVar) {
   console.log(i);
 }
