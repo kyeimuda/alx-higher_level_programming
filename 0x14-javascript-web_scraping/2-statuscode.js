cc477da8d202c7f19a286923df4d578efef1e1a3
kyeimuda <kyeibernard01@gmail.com>
2022-04-13 18:48:54 -0700

    2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request');
 
 request
-    .get(process.argv[2])
-    .on('response', function (response) {
-	console.log(`code: ${response.statusCode}`); // 200
-    });
+  .get(process.argv[2])
+  .on('response', function (response) {
+    console.log(`code: ${response.statusCode}`); // 200
+  });
