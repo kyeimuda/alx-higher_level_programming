74eb2c8463999a59692785851d73de1d6e3539a0
kyeimuda <kyeibernard01@gmail.com>
2022-04-13 19:14:51 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,18 +3,18 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-    if (error) {
-	console.log(error);
-    } else {
-	const movieInfo = JSON.parse(body);
-	let aux = 0;
-	for (const i of movieInfo.results) {
-	    for (const j of i.characters) {
-		if (j.search('18/') > 0) {
-		    aux++;
-		}
-	    }
-	}
-	console.log(aux);
+  if (error) {
+    console.log(error);
+  } else {
+    const movieInfo = JSON.parse(body);
+    let aux = 0;
+    for (const i of movieInfo.results) {
+      for (const j of i.characters) {
+        if (j.search('18/') > 0) {
+          aux++;
+        }
+      }
     }
+    console.log(aux);
+  }
 });
