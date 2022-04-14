465ad4a006adbf6d9c90f6ed43611a46faff8f7e
kyeimuda <kyeibernard01@gmail.com>
2022-04-13 19:07:11 -0700

    3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,11 +3,11 @@
 const request = require('request');
 
 request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
-	function (error, response, body) {
-	    if (error) {
-		console.log(error);
-	    } else {
-		const movieInfo = JSON.parse(body);
-		console.log(movieInfo.title);
-	    }
-	});
+  function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      const movieInfo = JSON.parse(body);
+      console.log(movieInfo.title);
+    }
+  });
