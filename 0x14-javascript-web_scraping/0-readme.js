5690f8bfa7d68f16375d586574412715e9811a53
kyeimuda <kyeibernard01@gmail.com>
2022-04-13 18:36:06 -0700

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 /* script that reads and prints the content of a file */
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-    if (err) {
-	return console.log(err);
-    }
-    console.log(data);
-})
+  if (err) {
+    return console.log(err);
+  }
+  console.log(data);
+});
