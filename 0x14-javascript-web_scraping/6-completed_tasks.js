a6fba1a8fa0df6d1f7014e7f79974f79d07010bd
kyeimuda <kyeibernard01@gmail.com>
2022-04-13 19:22:46 -0700

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,20 +3,20 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response, body) {
-    if (!error) {
-	const jsonFile = JSON.parse(body);
-	const taskComplete = {};
-	for (const i in jsonFile) {
-	    const data = jsonFile[i];
-	    if (data.completed === true) {
-		const id = data.userId;
-		if (id in taskComplete) {
-		    taskComplete[id]++;
-		} else {
-		    taskComplete[id] = 1;
-		}
-	    }
-	}
-	console.log(taskComplete);
+  if (!error) {
+    const jsonFile = JSON.parse(body);
+    const taskComplete = {};
+    for (const i in jsonFile) {
+      const data = jsonFile[i];
+      if (data.completed === true) {
+        const id = data.userId;
+        if (id in taskComplete) {
+          taskComplete[id]++;
+        } else {
+	  taskComplete[id] = 1;
+        }
+      }
     }
+    console.log(taskComplete);
+  }
 });
